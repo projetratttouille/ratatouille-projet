a46adb91ee3982433bd62bef40cccab65f5c5491
tanguilb <tangui.lebourdonnec@gmail.com>
2016-09-14 13:35:21 +0200

    Ajout de la photo de fond page d'accueil et du texte

--- index.php
+++ index.php
@@ -1,42 +1,17 @@
 <?php include('header.php'); ?>
 				
 <section>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+<div id="presentation">
+	<div class="image_de_fond">
+		<img src="images/restaurant-690569_1280.jpg" alt="légumes" />
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="images/01.jpg" alt="panier_legume">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="images/02.jpg" alt="image">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-  </div>
+		<div class="texte_presentation">
+			<p>La franchise ratatouille vous présente ses restaurants. Dans un esprits d’authenticité et de savoir faire traditionnelle, les différents restaurants vous proposent de savoureux plats réalisé à partir de produits locaux et de grandes qualités. Les restaurants ratatouille vous accueille du lundi au dimanche dans un cadre chaleureux et conviviale.</p>
+		</div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>				
+	</div>
 
+</div>
 <div class="restos">
 	<ul class="center-block">
 		<li><img src="images/restaurant_lachimere_gf.jpg" alt="restaurant de lyon" class="img-circle"/></li>
